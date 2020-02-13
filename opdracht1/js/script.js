7b77908c2ab2d7526ceadf6034409f91da7254bb
Chevelly <chevellyhiralall@re-byods-145-109-189-66.wireless.hva.nl>
2020-02-13 15:20:35 +0100

    upload

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,31 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+const listItems = document.querySelectorAll('.main li');
+const allimages = document.querySelectorAll('.main .gallery .images');
+
+function toggleActiveClass(active) {
+    listItems.forEach(item => {
+        item.classList.remove('active');
+    })
+    active.classList.add('active');
+}
+
+function toggleimages(dataClass) {
+    if (dataClass === 'all') {
+        for (let i = 0; i < allimages.length; i++) {
+            allimages[i].style.display = 'block';
+        }
+    } else {
+        for (let i = 0; i < allimages.length; i++)
+            allimages[i].dataset.class === dataClass ? allimages[i].style.display = 'block' : allimages[i].style.display = 'none';
+    }
+}
+
+for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('click', function () {
+        toggleActiveClass(listItems[i]);
+        toggleimages(listItems[i].dataset.class);
+    });
+}
