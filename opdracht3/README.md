6f0ec723a09ef319ba49dd46bd9b4b043e2b7b1b
Chevelly <chevellyhiralall@Chevellys-MacBook-Pro.local>
2020-03-25 01:35:18 +0100

    upload

--- opdracht3/README.md
+++ opdracht3/README.md
@@ -1,14 +1,4 @@
-Goedemorgen Sanne,
 
-Ik heb behoorlijk wat moeite met opdracht drie. Ik heb de tutorial doorlopen maar ik blijk toch veel moeite te hebben met een start maken aan mijn eigen ontwerp. Ik weet namelijk niet hoe ik het voor elkaar krijg. Kun jij mij misschien een tutorial aanraden die ik kan volgen? Of zou je een lesvideo op kunnen nemen waarbij je een voorbeeld geeft van de mogelijkheden?
-
-Zoals zichtbaar in mijn files ben ik absoluut niet op dreef maar ik blijf mijn best doen om toch iets te kunnen coderen. Hiervoor heb ik toch iets meer tijd nodig dus ik verwacht volgende week meer te kunnen laten zien.
-
-Stay safe!
-
-Groetjes,
-
-Chevelly
 
 ## Ontwerp
 Voor de eindopdracht wil een 'Quaranthemed Random Movie Generator' ontwerpen die de gebruiker een willekeurige film als suggestie laat zien om te bekijken tijdens deze vreemde en vervelende situatie waar we ons in bevinden. Het is een tool om de verveling voor velen tegen te gaan.
--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -1,20 +1,87 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
+/* de checkbox */
+var deCheckbox = document.querySelector("input");
+/* als er op geklikt wordt, wordt de functie haalMoviesOp uitgevoerd */
+deCheckbox.addEventListener("click", haalMoviesOp);
 
-let requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
+function haalMoviesOp() {
 
-let request = new XMLHttpRequest();
+    /* dit is de url waar je de data vandaan haalt */
+    let requestURL = "https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json";
 
-request.open('GET', requestURL);
+    /* de verbinding met de server maken */
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
 
-request.responseType = 'json';
-request.send();
+    /* het verzoek naar de server sturen */
+    request.send();
 
-request.onload = function () {
-    const movies = request.response;
-    showTitle(movies);
-    console.log(movies);
+    /* dit is de functie die wordt uitgevoerd als de server informatie terugstuurt */
+    request.onload = function () {
+
+        /* een status tussen 200 en 400 betekent dat de data ok is */
+        if (request.status >= 200 && request.status < 400) {
+            /* de response van de server (de data) wordt in een variabele gestopt */
+            var movies = request.response
+            /* de data is een array met daarin een lijst met films */
+            /* mogelijkheid tot loggen om de structuur en inhoud van de array te bekijken */
+            console.log(movies);
+            /* dan sturen we de data naar de functie renderMovies om de main te vullen */
+            renderMovies(movies);
+        }
+
+        /* een andere status betekent dat er iets mis is met de data */
+        else {
+            // We reached our target server, but it returned an error
+        }
+    };
+
+    /* dit is de functie die wordt uitgevoerd als de server een fout terugstuurt */
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+
+    function renderMovies(movies) {
+        /* doorloopt alle films in de array en keert een willekeurige film terug */
+        var randomMovie = Math.floor(Math.random() * (movies.length)); {
+
+            /* dit gebeurt voor elke film */
+            /* elementen aanmaken voor de HTML van de film */
+            let movie = document.createElement("article");
+            let title = document.createElement("h2");
+            let plot = document.createElement("p");
+            let poster = document.createElement("img");
+
+            /* de title vullen met de filminfo van de willekeurig gekozen film uit de array */
+            title.innerHTML = movies[randomMovie].title;
+            /* de plot vullen met de filminfo van de willekeurig gekozen film uit de array */
+            plot.innerHTML = movies[randomMovie].simple_plot;
+            /* de src van het plaatje vullen met de filminfo van de willekeurig gekozen film uit de array */
+            poster.src = movies[randomMovie].cover;
+
+            /* de elementen aan de movie (het article) toevoegen */
+            movie.appendChild(title);
+            movie.appendChild(plot);
+            movie.appendChild(poster);
+
+            /* de movie (het article) aan de main toevoegen */
+            document.body.querySelector("main").appendChild(movie);
+        }
+    }
 }
+
+/*
+let random = document.getElementById("random");
+// nieuwe node creeeren
+let main = document.createElement("main");
+main.textContent = movie;
+// het item vervangen
+
+random.replaceChild(main, random.firstElementChild);
+
+
+const header = document.querySelector("header");
+const section = document.querySelector("section");
 
 function showTitle(jsonObj) {
 
@@ -24,4 +91,4 @@ function showTitle(jsonObj) {
         title.textContent = jsonObj[i]['title'];
         header.appendChild(title);
     }
-}
+} */
